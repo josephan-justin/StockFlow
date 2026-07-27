b3b44fc2657477d7ff986a522f7c4f7e6abad9b8
josephan-justin <josephanjustin2@gmail.com>
2026-07-27 23:00:12 +0700

    feat : implement dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,7 +1,21 @@
+const { User, Product, Category, Supplier, Transaction } = require('../models')
+
 class Controller{
     static async dashboard(req, res){
         try {
-            res.render('dashboard')
+            const user = await User.findByPk(req.session.userId)
+            const totalProducts = await Product.count()
+            const totalCategories = await Category.count()
+            const totalSuppliers = await Supplier.count()
+            const totalTransactions = await Transaction.count()
+
+            res.render('dashboard', {
+                user, 
+                totalCategories, 
+                totalProducts, 
+                totalSuppliers, 
+                totalTransactions
+            })
         } catch (error) {
             console.log(error)
             res.send(error)
